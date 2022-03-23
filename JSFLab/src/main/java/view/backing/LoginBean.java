be83d0b4d2c636c9cf8a9c2d60a5aa86eb81028c
kaczmareka407 <kaczmareka407@gmail.com>
2022-03-23 10:23:40 +0100

    Completed laboratory tasks

--- JSFLab/src/main/java/view/backing/LoginBean.java
+++ JSFLab/src/main/java/view/backing/LoginBean.java
@@ -6,6 +6,10 @@ package view.backing;
 
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlSelectBooleanCheckbox;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 
 /**
  *
@@ -19,6 +23,8 @@ public class LoginBean
 
     private String username;
     private String password;
+    private HtmlSelectBooleanCheckbox acceptCheckbox;
+    private HtmlCommandButton loginButton;
 
     /**
      * Creates a new instance of LoginBean
@@ -69,4 +75,51 @@ public class LoginBean
             return "failure";
         }
     }
+
+    /**
+     * @return the acceptCheckbox
+     */
+    public HtmlSelectBooleanCheckbox getAcceptCheckbox()
+    {
+        return acceptCheckbox;
+    }
+
+    /**
+     * @return the loginButton
+     */
+    public HtmlCommandButton getLoginButton()
+    {
+        return loginButton;
+    }
+
+    public void activateButton(ValueChangeEvent e)
+    {
+        if (getAcceptCheckbox().isSelected())
+        {
+            getLoginButton().setDisabled(false);
+        } else
+        {
+            getLoginButton().setDisabled(true);
+        }
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
+    }
+
+    /**
+     * @param acceptCheckbox the acceptCheckbox to set
+     */
+    public void setAcceptCheckbox(HtmlSelectBooleanCheckbox acceptCheckbox)
+    {
+        this.acceptCheckbox = acceptCheckbox;
+    }
+
+    /**
+     * @param loginButton the loginButton to set
+     */
+    public void setLoginButton(HtmlCommandButton loginButton)
+    {
+        this.loginButton = loginButton;
+    }
+
 }
